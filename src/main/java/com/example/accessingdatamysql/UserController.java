d366a1cc0bdc9e1b04991692edcbfdfa220974fb
Potyv <poty.vinent@icloud.com>
2021-06-20 15:09:02 +0200

    1

---
+++ src/main/java/com/example/accessingdatamysql/UserController.java
@@ -0,0 +1,25 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/demo")
+
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email){
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+}
